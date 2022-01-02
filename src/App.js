b4bbbc5df3ba43b5c6123d6e244489afb7bced92
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-01-02 16:47:23 +0530

    disabled logHandler

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { HashRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact_us from "./components/pages/Contact_us";
 import About from "./components/pages/About";
 // import Insta_gal from "./components/pages/Insta_gal";
 import Profile from "./components/pages/Profile";
-import LogHandler from "./Handler/LogHandler";
+// import LogHandler from "./Handler/LogHandler";
 
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <div>
           {/* <h1>LETS MAKE THIS BRO</h1> */}
-          
+
           <Switch>
-            <Route exact path='/' component={Home} />
+            <Route exact path="/" component={Home} />
             {/* <Route exact path='/collection' component={Drive_links} /> */}
-            <Route exact path='/contact_us' component={Contact_us} />
+            <Route exact path="/contact_us" component={Contact_us} />
             {/* <Route exact path='/insta' component={Insta_gal} /> */}
-            <Route exact path='/about' component={About} />
-            <Route exact path='/profile' component={Profile}/>
-            <Route exact path='/login' component={LogHandler}/>
-        
+            <Route exact path="/about" component={About} />
+            <Route exact path="/profile" component={Profile} />
+            {/* <Route exact path='/login' component={LogHandler}/> */}
           </Switch>
-          
         </div>
       </div>
     </Router>
